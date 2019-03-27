d34278bcd57cfe43da3eb143c028d80615a4bfcb
JCPuchalt <48555488+JCPuchalt@users.noreply.github.com>
2019-03-27 16:51:55 +0100

    Update set_up_whole_system.sh

--- set_up_whole_system.sh
+++ set_up_whole_system.sh
@@ -136,17 +136,17 @@ install_opencv(){
 
   log "Downloading OpenCV..."
   git clone https://github.com/opencv/opencv.git
-  cd opencv
-  git checkout $cvVersion
-  cd ..
+  #cd opencv
+  #git checkout $cvVersion
+  #cd ..
 
   log "Downloading OpenCV_contrib..."
   git clone https://github.com/opencv/opencv_contrib.git
-  cd opencv_contrib
-  git checkout $cvVersion
-  cd ..
+  #cd opencv_contrib
+  #git checkout $cvVersion
+  #cd ..
 
-  cd ~
+  cd opencv
   mkdir build
   cd build
 
